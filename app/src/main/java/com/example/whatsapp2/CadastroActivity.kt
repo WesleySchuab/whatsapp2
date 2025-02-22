89a775a0e8915608e105ef686771d64763eb1a00
kaiza <kaizaconstroi@gmail.com>
2025-02-21 21:56:36 -0300

    configurando a toolbar na tela de CadastroActivity

--- app/src/main/java/com/example/whatsapp2/CadastroActivity.kt
+++ app/src/main/java/com/example/whatsapp2/CadastroActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.whatsapp2.databinding.ActivityCadastroBinding
 
 class CadastroActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityCadastroBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro)
+        setContentView(binding.root)
+        inicializarToolbar()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbar.materialToolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Cadastro"
     }
 }
--- app/src/main/java/com/example/whatsapp2/LoginActivity.kt
+++ app/src/main/java/com/example/whatsapp2/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.whatsapp2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,11 +17,20 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+        inicializarEventosClique()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun inicializarEventosClique() {
+        binding.textCadastro.setOnClickListener {
+            startActivity(
+                Intent(this, CadastroActivity::class.java)
+            )
         }
     }
 }
