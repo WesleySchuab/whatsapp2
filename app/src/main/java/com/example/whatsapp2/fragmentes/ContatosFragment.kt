e88dde90cde3b4b648852272a10fe86afc8a31b8
kaiza <kaizaconstroi@gmail.com>
2025-03-02 13:19:49 -0300

    Criando uma lista com os contatos existente

--- app/src/main/java/com/example/whatsapp2/fragmentes/ContatosFragment.kt
+++ app/src/main/java/com/example/whatsapp2/fragmentes/ContatosFragment.kt
@@ -1,18 +1,22 @@
 package com.example.whatsapp2.fragmentes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.whatsapp2.R
 import com.example.whatsapp2.databinding.FragmentContatosBinding
+import com.example.whatsapp2.model.Usuario
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 
 
 class ContatosFragment : Fragment() {
     private lateinit var binding: FragmentContatosBinding
+    private lateinit var eventoSnapshot: ListenerRegistration
     private val firebaseAuth by lazy {
         FirebaseAuth.getInstance()
     }
@@ -30,4 +34,40 @@ class ContatosFragment : Fragment() {
         return binding.root
         //return inflater.inflate(R.layout.fragment_contatos, container, false)
     }
+    override fun onStart() {
+        super.onStart()
+        adicionarListenerContatos()
+    }
+
+    private fun adicionarListenerContatos() {
+         eventoSnapshot = firestore
+            .collection("usuarios")
+            .addSnapshotListener { querySnapshot, erro ->
+
+               val documento = querySnapshot?.documents
+
+                documento?.forEach { documentSnapshot ->
+
+                    val listaContatos = mutableListOf<Usuario>()
+                    val usuario = documentSnapshot.toObject(Usuario::class.java)
+
+                    if(usuario != null){
+                        if(usuario.id != firebaseAuth.currentUser?.uid){
+                            Log.i("ContatosFragment", "adicionarListenerContatos: ${usuario.nome} ")
+                            val idUsuario = firebaseAuth.currentUser?.uid
+                            if (idUsuario != null) {
+                                listaContatos.add(usuario)
+                                //binding.recyclerContatos.adapter = ContatosAdapter(listaContatos)
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        eventoSnapshot.remove()
+    }
 }
+
--- app/src/main/java/com/example/whatsapp2/model/Usuario.kt
+++ app/src/main/java/com/example/whatsapp2/model/Usuario.kt
@@ -1,8 +1,8 @@
 package com.example.whatsapp2.model
 
 data class Usuario(
-    val id: String ,
-    val nome: String,
-    val email: String,
+    val id: String = "" ,
+    val nome: String = "",
+    val email: String = "",
     val foto: String = ""
 )
