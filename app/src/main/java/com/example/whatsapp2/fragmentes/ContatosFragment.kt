d227fa1c880c8f3f1f6fc068c0fe0a393acad95b
kaiza <kaizaconstroi@gmail.com>
2025-03-02 11:10:38 -0300

    Configurando viewbinding no fragmento Contatos

--- app/src/main/java/com/example/whatsapp2/fragmentes/ContatosFragment.kt
+++ app/src/main/java/com/example/whatsapp2/fragmentes/ContatosFragment.kt
@@ -6,9 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.whatsapp2.R
+import com.example.whatsapp2.databinding.FragmentContatosBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class ContatosFragment : Fragment() {
+    private lateinit var binding: FragmentContatosBinding
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
 
 
     override fun onCreateView(
@@ -16,6 +26,8 @@ class ContatosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false)
+        binding = FragmentContatosBinding.inflate(inflater, container, false)
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_contatos, container, false)
     }
 }
