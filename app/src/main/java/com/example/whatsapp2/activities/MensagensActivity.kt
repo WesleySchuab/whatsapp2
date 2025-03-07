ae5352f8d30c3a9fffad9a2dd16b0bab5f38232f
Wesley Schuab <yinnihisoka@gmail.com>
2025-03-07 14:27:53 -0300

    Listando as conversas

--- app/src/main/java/com/example/whatsapp2/activities/MensagensActivity.kt
+++ app/src/main/java/com/example/whatsapp2/activities/MensagensActivity.kt
@@ -217,13 +217,11 @@ class MensagensActivity : AppCompatActivity() {
 
         val extras = intent.extras
         if (extras != null) {
-            val origem = extras.getString("origem")
-            if (origem == Constantes.ORIGEM_CONTATO) {
-                dadosDestinatario = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    extras.getParcelable("dadosDestinatario", Usuario::class.java)
-                } else {
-                    extras.getParcelable("dadosDestinatario")
-                }
+           // val origem = extras.getString("origem")
+            dadosDestinatario = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                extras.getParcelable("dadosDestinatario", Usuario::class.java)
+            } else {
+                extras.getParcelable("dadosDestinatario")
             }
 
         }
--- app/src/main/java/com/example/whatsapp2/fragmentes/ConversasFragment.kt
+++ app/src/main/java/com/example/whatsapp2/fragmentes/ConversasFragment.kt
@@ -1,13 +1,19 @@
 package com.example.whatsapp2.fragmentes
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.whatsapp2.activities.MensagensActivity
+import com.example.whatsapp2.adapters.ConversasAdapter
 import com.example.whatsapp2.databinding.FragmentConversasBinding
 import com.example.whatsapp2.model.Conversa
+import com.example.whatsapp2.model.Usuario
 import com.example.whatsapp2.utils.Constantes
 import com.example.whatsapp2.utils.exibirMensagem
 import com.google.firebase.auth.FirebaseAuth
@@ -27,14 +33,38 @@ class ConversasFragment : Fragment() {
         FirebaseFirestore.getInstance()
     }
 
+    private lateinit var conversasAdapter: ConversasAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentConversasBinding.inflate(inflater, container, false)
+        //Instancia o adapter
+        conversasAdapter = ConversasAdapter { conversa ->
+            val intent = Intent(context, MensagensActivity::class.java)
+
+            val usuario = Usuario(
+                id = conversa.idUsuarioDestinatario,
+                nome = conversa.nome,
+                foto = conversa.foto
+            )
+            intent.putExtra("dadosDestinatario", usuario)
+            //intent.putExtra("origem", Constantes.ORIGEM_CONVERSA)
+            startActivity(intent)
+        }
+        binding.rvConversa.adapter = conversasAdapter
+        binding.rvConversa.layoutManager = LinearLayoutManager(context)
+        binding.rvConversa.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                LinearLayoutManager.VERTICAL
+            )
+        )
         return binding.root
     }
+
 
     override fun onStart() {
         super.onStart()
@@ -50,7 +80,7 @@ class ConversasFragment : Fragment() {
     private fun adicionarListenerConversas() {
         val idUsuarioRemetente = firebaseAuth.currentUser?.uid
         if (idUsuarioRemetente != null) {
-           // activity?.exibirMensagem("Usuário encontrado")
+            // activity?.exibirMensagem("Usuário encontrado")
             eventoSnapshot = firestore
                 .collection(Constantes.CONVERSAS)
                 .document(idUsuarioRemetente)
@@ -73,10 +103,11 @@ class ConversasFragment : Fragment() {
                     }
                     if (listaConversas.isNotEmpty()) {
                         // Atualiza o adapter com a lista de conversas
+                        conversasAdapter.adicionarLista(listaConversas)
 
                     }
                 }
-        }else{
+        } else {
             activity?.exibirMensagem("Usuário não encontrado")
 
         }
